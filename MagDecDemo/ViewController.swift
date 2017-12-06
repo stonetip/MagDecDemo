650ab417649627febc1eb151d97d8822067907d1
Jon Nehring <stonetip@live.com>
2017-12-06 11:49:44 -0700

    Initial Commit

---
+++ MagDecDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MagDecDemo
+//
+//  Created by Jonathan Nehring on 12/6/17.
+//  Copyright © 2017 Jonathan Nehring. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
